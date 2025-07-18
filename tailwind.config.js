5d5c942ee7a379f7311c4bff1aa11c473a0d9af2
hamidulloh-dev <hamidullohmuhammadjonov123@gmail.com>
2025-07-18 18:13:32 +0500

    1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const config = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "fon-1": "url('./img/Container.svg')",
+      },
+    },
+  },
+};
+export default config;
